8c6b288abaca0539b5ae140bf2cb7e663b7039e1
aledic85 <dicuollo85@gmail.com>
2019-03-01 17:48:42 +0100

    adding the script to make the menu disappear if the window is resized

--- script.js
+++ script.js
@@ -3,3 +3,10 @@
 $(".hamb-menu").click(function() {
   $(".hidden-nav").toggle(1000);
 });
+
+// FACCIO SCOMPARIRE L'HAMBURGER MENU IN CASO L'UTENTE NON CLICCA
+// SULL'ICONCINA E INGRANDISCE LA SCHERMATA;
+
+$(window).resize(function(){
+  $(".hidden-nav").css("display", "none");
+});
